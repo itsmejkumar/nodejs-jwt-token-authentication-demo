723463d0d8b3e00531516adc1ab8cf411264043c
Jayakumar <jayakumar.vijay@everestdx.com>
2020-09-16 23:10:27 +0530

    Issue fixes

---
+++ api/users/user.service.js
@@ -0,0 +1,97 @@
+const pool = require("../../config/database");
+
+module.exports = {
+  getUserByUserEmail: (email, callBack) => {
+    pool.query(
+      `select * from users where email=?`,
+      [
+        email,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  create: (data, callBack) => {
+    pool.query(
+      `insert into users(firstName, lastName, gender, email, password, number) values(?,?,?,?,?,?)`,
+      [
+        data.firstName,
+        data.lastName,
+        data.gender,
+        data.email,
+        data.password,
+        data.number,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  getUsers: (callBack) => {
+    pool.query(
+      `select * from users`,
+      [],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  getUserById: (id, callBack) => {
+    pool.query(
+      `select * from users where id=?`,
+      [
+        id,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  updateUser: (data, callBack) => {
+    pool.query(
+      `update users set firstName=?, lastName=?, gender=?, email=?, password=?, number=? where id=?`,
+      [
+        data.firstName,
+        data.lastName,
+        data.gender,
+        data.email,
+        data.password,
+        data.number,
+        data.id
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  deleteUser: (id, callBack) => {
+    pool.query(
+      `delete from users where id=?`,
+      [
+        id,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+};
